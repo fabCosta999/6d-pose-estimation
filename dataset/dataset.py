3bb284ec96f0fd5e1d2829843e164944130715ee
Fabrizio Costa s337191 <s337191@studenti.polito.it>
2025-12-13 12:13:50 +0100

    datset

---
+++ dataset/dataset.py
@@ -0,0 +1,70 @@
+import torch
+from torch.utils.data import Dataset
+from pathlib import Path
+import yaml
+import torchvision.transforms as transforms
+from PIL import Image
+
+class LinemodDataset(Dataset):
+    CLASSES = [1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15]
+    OBJ_ID_TO_CLASS = {obj_id: i for i, obj_id in enumerate(CLASSES)}
+
+    def __init__(self, dataset_root, split="train"):
+        self.dataset_root = Path(dataset_root)
+        self.split = split
+
+        self.samples = []
+        self.gt_data = {}
+
+        for obj_id in self.CLASSES:
+            obj_dir = self.dataset_root / "data" / f"{obj_id:02d}"
+
+            split_file = obj_dir / f"{split}.txt"
+            with open(split_file) as f:
+                for line in f:
+                    self.samples.append((obj_id, int(line.strip())))
+
+            with open(obj_dir / "gt.yml") as f:
+                self.gt_data[obj_id] = yaml.safe_load(f)
+
+        self.transform = transforms.Compose([
+            transforms.Resize((640, 640)),
+            transforms.ToTensor(),
+        ])
+
+    def convert_bb_yolo(self, bb, W, H):
+        x, y, w, h = bb
+        xc = x + w / 2
+        yc = y + h / 2
+        xc /= W
+        yc /= H
+        w  /= W
+        h  /= H
+        return torch.tensor([xc, yc, w, h], dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        obj_id, img_id = self.samples[idx]
+
+        img_path = (
+            self.dataset_root / "data" / f"{obj_id:02d}" /
+            "rgb" / f"{img_id:04d}.png"
+        )
+
+        img = Image.open(img_path).convert("RGB")
+        W, H = img.size
+        img = self.transform(img)
+
+        gt_entry = self.gt_data[obj_id][img_id][0]
+        bb = gt_entry["obj_bb"]
+        class_id = self.OBJ_ID_TO_CLASS[gt_entry["obj_id"]]
+
+        bbox = self.convert_bb_yolo(bb, W, H)
+
+        return {
+            "rgb": img,
+            "bbox": bbox,
+            "class_id": class_id,
+        }
