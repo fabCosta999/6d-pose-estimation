eea70c95242869e706b20c03ff4ab491b2e3aab3
Fabrizio Costa s337191 <s337191@studenti.polito.it>
2025-12-28 21:48:40 +0100

    yolo train for just ten epochs

--- train_yolo.py
+++ train_yolo.py
@@ -5,12 +5,12 @@ model = YOLO("yolo11s.pt")
 results = model.train(
     data="/content/6d-pose-estimation/data/dataset_yolo/data.yaml",
     imgsz=640,
-    epochs=50,
+    epochs=10,
     batch=16,
     freeze=10,
     workers=4,
     project="/content/drive/MyDrive/machine_learning_project",
-    name="yolo_pose_exp1"
+    name="yolo_8020"
 )
 print(results)
 print(results.save_dir)
